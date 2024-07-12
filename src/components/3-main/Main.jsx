b9a61031d7d94eac5988ae18abd8f06f88e63837
Rafat Hanna <109249648+rafathanna@users.noreply.github.com>
2024-07-12 23:38:51 +0300

    Update Main.jsx

--- src/components/3-main/Main.jsx
+++ src/components/3-main/Main.jsx
@@ -2,10 +2,10 @@ import { useState } from "react";
 import "./main.css";
 
 const initialProjects = [
-  { id: 1, imgPath: "/Blood.png", proTitle: "Blood Bank System", proDescribe: "", category: "arr", link: "https://github.com/rafathanna/Blood-Bank-system" },
-  { id: 2, imgPath: "/To do list.png", proTitle: "To Do List", proDescribe: "", category: "arr", link: "https://github.com/rafathanna/TO-DO-LIST" },
-  { id: 4, imgPath: "ecc.PNG", proTitle: "Ecommerce project", proDescribe: "", category: "arr", link: "https://github.com/rafathanna/Ecommerce-Website" },
-  { id: 5, imgPath: "image.png", proTitle: "Courses Website", proDescribe: "", category: "arr", link: "https://github.com/rafathanna/Courses-website" },
+  { id: 1, imgPath: "/zz.png", proTitle: "https://rafathanna.github.io/dashboard/", proDescribe: "", category: "arr", link: "https://rafathanna.github.io/dashboard/" },
+  { id: 2, imgPath: "/To do list.png", proTitle: "To Do List", proDescribe: "", category: "arr", link: "https://rafathanna.github.io/TO-DO-LIST/" },
+  { id: 4, imgPath: "ecc.PNG", proTitle: "Ecommerce project", proDescribe: "", category: "arr", link: "ecommerce-website-fe7c7.web.app/" },
+  { id: 5, imgPath: "image.png", proTitle: "Courses Website", proDescribe: "", category: "arr", link: " https://rafathanna.github.io/Courses-website/" },
   { id: 3, imgPath: "/7.jpg", proTitle: "Expenses Project", proDescribe: "", category: "arr", link: "https://github.com/rafathanna/Expenses-Project" },
   { id: 6, imgPath: "Capture.PNG", proTitle: "My Cv", proDescribe: "", category: "cv", link: "https://drive.google.com/file/d/1isOUbMDMfevFA5n2rNpgqPfXCsB8lPN7/view?usp=sharing" },
   { id: 7, imgPath: "cer1 (1).jpeg", proTitle: "Certificate  1", proDescribe: "", category: "ce", link: "https://drive.google.com/file/d/18Ehh_qi_0k1QA-IbPRBpcSvtsOc1x8Fs/view?usp=sharing" },
